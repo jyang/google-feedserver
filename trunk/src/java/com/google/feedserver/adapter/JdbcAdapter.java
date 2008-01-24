84942e3aed9e166eda2ca0da11ababd0c39c1af4
vasunori <vasunori@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2008-01-24 23:20:08 +0000

    Fixing this bug: namespace extension declaration is incorrectly constructed.

--- trunk/src/java/com/google/feedserver/adapter/JdbcAdapter.java
+++ trunk/src/java/com/google/feedserver/adapter/JdbcAdapter.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -63,7 +63,7 @@ public class JdbcAdapter extends AbstractAdapter implements Adapter {
     String queryId = feedId + "-get-feed";
     List<Map<String, Object>> rows = client.queryForList(queryId);
     Feed feed = createFeed();
-    feed.declareNS(GBASE_NS_PREFIX, GBASE_NS);
+    feed.declareNS(GBASE_NS, GBASE_NS_PREFIX);
     for (Map<String, Object> row : rows) {
       Entry entry = createEntryFromRow(row);
       feed.addEntry(entry);
