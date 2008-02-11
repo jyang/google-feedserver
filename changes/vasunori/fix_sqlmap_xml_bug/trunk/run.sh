09056bdbdd506b8218e1ed102a94a98d10889531
vasunori <vasunori@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2008-02-11 20:08:20 +0000

    fixing bugs in sqlmap.xml

---
+++ changes/vasunori/fix_sqlmap_xml_bug/trunk/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# build up a classpath containing all the jars in lib/ and dist/
+for jar in lib/*.jar dist/*.jar; do
+  CLASSPATH=${CLASSPATH}:$jar
+done
+
+#add "conf" dir to CLASSPATH. thats where adapter and feedserver config files are at.
+CLASSPATH=$CLASSPATH:conf
+
+# number of args can be 0 or 2. not 1.
+# because in SimpleCommandLineParser.java file, it sets port, uri separately. For these 2 to be
+# in sync with each other, either the user supplies both or none.
+# and run jetty.Main
+
+if [ $# -eq 1 ]
+then
+   if [ $1 == "help" ]
+   then
+     echo "Usage: $0 --port=<number> --uri=http://host:port/"
+     exit 0
+   fi
+fi
+java -classpath $CLASSPATH com.google.feedserver.server.jetty.Main $*
