da3ab9bd2856eb53a037ae541233b14ea7f6545f
vasunori <vasunori@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2008-02-14 19:09:30 +0000

    default value for "uri" should not end with "/". if it does, the entry id's will have extra "/" like this          http://host:port//feed/entryid instead of          http://host:port/feed/entryid

--- src/java/com/google/feedserver/config/ServerConfiguration.java
+++ src/java/com/google/feedserver/config/ServerConfiguration.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,20 +29,17 @@ public class ServerConfiguration extends Configuration {
   private int port = 8080;
 
   private static final String OPTION_FEED_SERVER_URI = "uri";
-  private String uri = "http://localhost:8080/";
+  private String uri = "http://localhost:8080";
 
   private static final String OPTION_FEED_NS = "namespace";
   private String namespace = "http://feeds.yourdomain.com/ns/1.0";
 
-  private static final String OPTION_ENTRY_PREFIX = "nsPrefix";
-  private String nsPrefix = "g";
-
   private static final String OPTION_FEED_CONFIG_LOCATION = "feedConfigLocation";
-  private String feedConfigLocation = "conf/feedserver/adapter/";
+  private final String feedConfigLocation = "conf/feedserver/adapter/";
 
   private static final String OPTION_FEED_ADAPTER_CONFIG_LOCATION
       = "feedAdapterConfigLocation";
-  private String feedAdapterConfigLocation = "feedserver/";
+  private final String feedAdapterConfigLocation = "feedserver/";
 
   protected static final String PROPERTIES_FILE_SUFFIX = ".properties";
 
@@ -50,7 +47,7 @@ public class ServerConfiguration extends Configuration {
 
   private static final ServerConfiguration serverConfiguration
       = new ServerConfiguration();
-  
+
   public void initialize(SimpleCommandLineParser parser) {
     if (null != parser) {
       if (parser.containsKey(OPTION_PORT)) {
@@ -61,9 +58,6 @@ public class ServerConfiguration extends Configuration {
       }
       if (parser.containsKey(OPTION_FEED_NS)) {
         namespace = parser.getValue(OPTION_FEED_NS);
-      }
-      if (parser.containsKey(OPTION_ENTRY_PREFIX)) {
-        nsPrefix = parser.getValue(OPTION_ENTRY_PREFIX);
       }
       if (parser.containsKey(OPTION_FEED_CONFIG_LOCATION)) {
         nsPrefix = parser.getValue(OPTION_FEED_CONFIG_LOCATION);
@@ -85,15 +79,11 @@ public class ServerConfiguration extends Configuration {
   public String getServerUri() {
     return uri;
   }
-  
+
   public String getFeedNamespace() {
     return namespace;
   }
 
-  public String getFeedNamespacePrefix() {
-    return nsPrefix;
-  }
-  
   public String getFeedConfigLocation() {
     return feedConfigLocation;
   }
