1a46b4b5b643e7c7b377fddbc391a42af95851eb
r@kuci.org <r@kuci.org@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2008-11-13 01:27:05 +0000

    Small logic fix missed in last CL.

---
+++ changes/r@kuci.org/u2r7/trunk/src/java/com/google/feedserver/client/FeedServerEntry.java
@@ -0,0 +1,161 @@
+/* Copyright 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.feedserver.client;
+
+import com.google.feedserver.util.ContentUtil;
+import com.google.feedserver.util.FeedServerClientException;
+import com.google.gdata.data.BaseEntry;
+import com.google.gdata.data.OtherContent;
+import com.google.gdata.util.XmlBlob;
+
+import org.xml.sax.SAXException;
+
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Represents a "payload-in-content" feed entry.  Simplifies interaction with feeds originating
+ * from FeedServer and FeedStore.
+ * 
+ * @author r@kuci.org (Ray Colline)
+ */
+public class FeedServerEntry extends BaseEntry<FeedServerEntry> {
+  
+
+  /**
+   * Creates empty entry.
+   */
+  public FeedServerEntry() {}
+  
+  /**
+   * Creates entry from provided entity bean.
+   * 
+   * @param entity a populated bean.
+   */
+  public FeedServerEntry(Object entity) {
+    setContentFromEntity(entity);
+  }
+  
+  /**
+   * Creates entry from provided populated content.
+   * 
+   * @param content populated content.
+   */
+  public FeedServerEntry(OtherContent content) {
+    this.setContent(content);
+  }
+  
+  /**
+   * Creates entry from "entity" XML.  
+   * 
+   * Example:
+   * <pre>
+   *   <entity>
+   *     <name>vehicle0</name>
+   *     <owner>Joe</owner>
+   *     <price>23000</price>
+   *     <propertyName repeatable='true'>make</propertyName>
+   *     <propertyName>model</propertyName>
+   *     <propertyName>year</propertyName>
+   *     <propertyValue repeatable='true'>Honda</propertyValue>
+   *     <propertyValue>Civic Hybrid</propertyValue>
+   *     <propertyValue>2007</propertyValue>
+   *   </entity>
+   * </pre>
+   * 
+   * @param xmlText XML string representing an entity.
+   */
+  public FeedServerEntry(String xmlText) {
+    ContentUtil contentUtil = new ContentUtil();
+    XmlBlob xmlBlob = new XmlBlob();
+    xmlBlob.setBlob(xmlText);
+    setXmlBlob(xmlBlob);
+    setContent(contentUtil.createXmlContent(xmlText));
+  }
+  
+  /**
+   * Sets the content of this entry from the supplied entity bean.
+   * 
+   * @param entity a bean representing the content.
+   */
+  public void setContentFromEntity(Object entity) {
+    ContentUtil contentUtil = new ContentUtil();
+    try {
+      OtherContent content = contentUtil.createXmlContent(entity);
+      this.setXmlBlob(content.getXml());
+      this.setContent(content);
+    } catch (IllegalArgumentException e) {
+      throw new RuntimeException("Invalid entity bean " + entity.getClass().getName(), e);
+    } catch (IntrospectionException e) {
+      throw new RuntimeException("Invalid entity bean " + entity.getClass().getName(), e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("Invalid entity bean " + entity.getClass().getName(), e);
+    } catch (InvocationTargetException e) {
+      throw new RuntimeException("Invalid entity bean " + entity.getClass().getName(), e);
+    }
+  }
+  
+  /**
+   * Utility method that given XML source for a feed entry, populates the content of this entry.
+   * 
+   * @param xmlText XML source for the associated feed entry.
+   * @throws FeedServerClientException if any conversion errors occur parsing the XML.
+   */
+  public void setContentFromXmlString(String xmlText) throws FeedServerClientException {
+    ContentUtil contentUtil = new ContentUtil();
+    XmlBlob xmlBlob = new XmlBlob();
+    xmlBlob.setBlob(xmlText);
+    setXmlBlob(xmlBlob);
+    setContent(contentUtil.createXmlContent(xmlText));
+  }
+  
+  /**
+   * Returns a populated entity bean from the provided entity class template.  The bean
+   * should a field for each part of the entity you wish to retrieve. 
+   * 
+   * @param <T> the entity bean.
+   * @param entityClass the bean template class. 
+   * @return a populated entity ean.
+   * @throws FeedServerClientException if the content in the entry is invalid
+   * @throws RuntimeException if the bean is invalid.
+   */
+  public <T> T getEntity(Class<T> entityClass) throws FeedServerClientException {
+    try {
+      ContentUtil contentUtil = new ContentUtil();
+      T entity = entityClass.newInstance();
+      contentUtil.fillBean((OtherContent) this.getContent(), entity);
+      return entity;
+    } catch (IllegalArgumentException e) {
+      throw new RuntimeException("Invalid bean " + entityClass.getName(), e);
+    } catch (SAXException e) {
+      throw new FeedServerClientException(e);
+    } catch (ParserConfigurationException e) {
+      throw new RuntimeException("Invalid XML handler", e);
+    } catch (IntrospectionException e) {
+      throw new RuntimeException("Invalid bean " + entityClass.getName(), e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("Invalid bean " + entityClass.getName(), e);
+    } catch (InvocationTargetException e) {
+      throw new RuntimeException("Invalid bean " + entityClass.getName(), e);
+    } catch (InstantiationException e) {
+      throw new RuntimeException("Could not instantiate bean class" + entityClass.getName());
+    } catch (IOException e) {
+      throw new FeedServerClientException(e);
+    }
+  }
+}
---
+++ changes/r@kuci.org/u2r7/trunk/src/java/com/google/feedserver/testing/FeedServerClientTestUtil.java
@@ -0,0 +1,202 @@
+/* Copyright 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.feedserver.testing;
+
+import com.google.feedserver.client.FeedServerEntry;
+import com.google.feedserver.tools.SampleFeedTool.VehicleBean;
+import com.google.feedserver.util.ContentUtil;
+import com.google.feedserver.util.FeedServerClientException;
+import com.google.gdata.data.Entry;
+import com.google.gdata.data.OtherContent;
+import com.google.gdata.util.XmlBlob;
+
+import org.easymock.IArgumentMatcher;
+import org.easymock.classextension.EasyMock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Provides data and utilities for testing the feedserver clients.
+ * 
+ * @author r@kuci.org (Ray Colline)
+ */
+public class FeedServerClientTestUtil {
+  
+  public static final String NAME = "vehicle0";
+  public static final String OWNER = "Joe";
+  public static final String PRICE = "23000";
+  public static final String[] PROPERTY_NAMES = { "make", "model", "year" };
+  public static final String[] PROPERTY_VALUES = { "Honda", "Civic Hybrid", "2007" };
+  
+  public static final String ENTRY_XML = 
+    "<entity>\n" +
+    "  <name>vehicle0</name>\n" +
+    "  <owner>Joe</owner>\n" +
+    "  <price>23000</price>\n" +
+    "  <propertyName repeatable='true'>make</propertyName>\n" +
+    "  <propertyName>model</propertyName>\n" +
+    "  <propertyName>year</propertyName>\n" +
+    "  <propertyValue repeatable='true'>Honda</propertyValue>\n" +
+    "  <propertyValue>Civic Hybrid</propertyValue>\n" +
+    "  <propertyValue>2007</propertyValue>\n" +
+    "</entity>\n";
+  
+  private VehicleBean sampleVehicleBean;
+  private Map<String, Object> sampleVehicleMap;
+  private FeedServerEntry vehicleEntry;
+  
+  /**
+   * Populates bean with static info to match XML.  The XML and the bean should
+   * be interchangable using utilities provided by FeedServerClient.
+   */
+  public FeedServerClientTestUtil() {
+    // Setup sample bean.
+    sampleVehicleBean = new VehicleBean();
+    sampleVehicleBean.setName(NAME);
+    sampleVehicleBean.setOwner(OWNER);
+    sampleVehicleBean.setPrice(PRICE);
+    sampleVehicleBean.setPropertyName(PROPERTY_NAMES);
+    sampleVehicleBean.setPropertyValue(PROPERTY_VALUES);
+    
+    // Setup sample map.
+    sampleVehicleMap = new HashMap<String, Object>();
+    sampleVehicleMap.put("name", NAME);
+    sampleVehicleMap.put("owner", OWNER);
+    sampleVehicleMap.put("price", PRICE);
+    sampleVehicleMap.put("propertyName", PROPERTY_NAMES);
+    sampleVehicleMap.put("propertyValue", PROPERTY_VALUES);
+    
+    // Create populated gdata entry.
+    XmlBlob xmlBlob = new XmlBlob();
+    xmlBlob.setBlob(ENTRY_XML);
+    OtherContent xmlContent = new OtherContent();
+    xmlContent.setXml(xmlBlob);
+    xmlContent.setMimeType(ContentUtil.APPLICATION_XML);
+    vehicleEntry = new FeedServerEntry();
+    vehicleEntry.setXmlBlob(xmlBlob);
+    vehicleEntry.setContent(xmlContent);
+  }
+  
+  /**
+   * Compares two vehicle beans to see if they are identical.
+   * 
+   * @param expected the expected bean.
+   * @param actual the actual bean.
+   * @return true if they are identical, false otherwise.
+   */
+  public boolean isEqual(VehicleBean expected, VehicleBean actual) {
+    if (expected.getName().equals(actual.getName()) && 
+        expected.getOwner().equals(actual.getOwner()) &&
+        expected.getPrice().equals(actual.getPrice())) {
+      // In the vehicle example the amount of property names and property values is identical.
+      // They are also in determistic order.
+      for (int index=0; index < expected.getPropertyName().length; index++) {
+        if ((!expected.getPropertyName()[index].equals(actual.getPropertyName()[index])) ||
+            (!expected.getPropertyValue()[index].equals(actual.getPropertyValue()[index]))) {
+          return false;
+        }
+      } 
+      return true;
+    }
+    return false;
+  }
+  
+  /**
+   * Compares two typeless maps to see if they are identical.
+   * 
+   * @param expected the expected entry represented as a map.
+   * @param actual the actual entry represented as a map.
+   * @return true if they are identical, false otherwise.
+   * @throws NullPointerException if map isnt properly formatted
+   * @throws ClassCastException if map is not of "typeless" variety.
+   * @throws IndexOutOfBoundsException if actual map has less entries than expected map.
+   */
+  public boolean isEqual(Map<String, Object> expected, Map<String, Object> actual) {
+    for (String key : expected.keySet()) {
+      if (expected.get(key) instanceof String) {
+        if (!expected.get(key).equals(actual.get(key))) {
+          return false;
+        }
+      } else if (expected.get(key) instanceof Object[]) {
+        Object[] expectedRepeatedValue = (Object[]) expected.get(key);
+        Object[] actualRepeatedValue = (Object[]) actual.get(key);
+        for (int index=0 ; index < expectedRepeatedValue.length ; index++) {
+          if (!((String) expectedRepeatedValue[index]).equals(
+              actualRepeatedValue[index])) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  public VehicleBean getSampleVehicleBean() {
+    return sampleVehicleBean;
+  }
+  
+  public FeedServerEntry getVehicleEntry() {
+    return vehicleEntry;
+  }
+  
+  public Map<String, Object> getSampleVehicleMap() {
+    return sampleVehicleMap;
+  }
+  
+  /**
+   * Easymock helper function to compare {@link Entry} objects.
+   * 
+   * @param in object to compare.
+   * @return an Entry to pass through.
+   */
+  public static FeedServerEntry eqEntry(FeedServerEntry in) {
+    EasyMock.reportMatcher(new EntryEquals(in));
+    return null;
+  }
+  
+  /**
+   * Entry equivalence argument matcher for easymock to check {@link Entry} objects.
+   * 
+   * @author rayc@google.com (Ray Colline)
+   */
+  public static class EntryEquals implements IArgumentMatcher {
+    
+    private FeedServerEntry expected;
+    
+    public EntryEquals(FeedServerEntry expected) {
+      this.expected = expected;
+    }
+    
+    public boolean matches(Object actual) {
+      if (!(actual instanceof FeedServerEntry)) {
+        return false;
+      }
+      try {
+        return new FeedServerClientTestUtil().isEqual(
+            expected.getEntity(VehicleBean.class),
+            ((FeedServerEntry) actual).getEntity(VehicleBean.class));
+      } catch (FeedServerClientException e) {
+        return false; 
+      }
+    }
+  
+    
+    public void appendTo(StringBuffer buffer) {
+      buffer.append("eqException( expected: ");
+      buffer.append(expected.getXmlBlob().getBlob());
+    }
+  }
+}
