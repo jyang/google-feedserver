b0a44e776c96ddfd642cdd66c5507be90640c79c
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-01-29 22:23:49 +0000

    fixed iframe content creation logic

--- src/gadgets/private-gadget-editor/codepress.js
+++ src/gadgets/private-gadget-editor/codepress.js
@@ -60,48 +60,54 @@ CodePress = function(obj) {
     if(!self.textarea.disabled) return;
     self.language = language ? language : self.options.replace(/ ?codepress ?| ?readonly-on ?| ?autocomplete-off ?| ?linenumbers-off ?/g,'');
     if(!CodePress.languages[self.language]) self.language = 'generic';
-    self.innerHTML = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' +
-        '<html>' +
-        '<head>' +
-        '	<title>CodePress - Real Time Syntax Highlighting Editor written in JavaScript</title>' +
-        '	<meta name="description" content="CodePress - source code editor window" />' +
-        '' +
-        '	<script type="text/javascript">' +
-        '	var language = "gm";' +
-        '	var engine = "older";' +
-        '	var ua = navigator.userAgent;' +
-        '	var ts = (new Date).getTime(); // timestamp to avoid cache' +
-        '	var lh = location.href;' +
-        '	' +
-        '	if(ua.match("MSIE")) engine = "msie";' +
-        '	else if(ua.match("KHTML")) engine = "khtml";' +
-        '	else if(ua.match("Opera")) engine = "opera";' +
-        '	else if(ua.match("Gecko")) engine = "gecko";' +
-        '' +
-        '	if(lh.match("language=")) language = lh.replace(/.*language=(.*?)(&.*)?$/,"$1");' +
-        '	' +
-        '	// XSS fix (wwen)' +
-        '	// these are the only languages that GGE supports' +
-        '	if (!language.match(/^(css|gadget|javascript)$/)) {' +
-        '	  language = "gadget";' +
-        '	}' +
-        '' +
-        '	document.write(\'<link type="text/css" href="' + CodePress.path + 'codepress.css?ts=\'+ts+\'" rel="stylesheet" />\');' +
-        '	document.write(\'<link type="text/css" href="' + CodePress.path + 'languages/\'+language+\'.css?ts=\'+ts+\'" rel="stylesheet" id="cp-lang-style" />\');' +
-        '	document.write(\'<scr\'+\'ipt type="text/javascript" src="' + CodePress.path + 'engines/\'+engine+\'.js?ts=\'+ts+\'"></scr\'+\'ipt>\');' +
-        '	document.write(\'<scr\'+\'ipt type="text/javascript" src="' + CodePress.path + 'languages/\'+language+\'.js?ts=\'+ts+\'"></scr\'+\'ipt>\');' +
-        '	</script>' +
-        '' +
-        '</head>' +
-        '' +
-        '<script type="text/javascript">' +
-        'if (engine == "gecko") document.write("<body> </body>");' +
-        'else if(engine == "msie") document.write("<body><pre></pre></body>");' +
-        'else if(engine == "opera") document.write("<body></body>");' +
-        '// else if(engine == "khtml") document.write("<body> </body>");' +
-        '</script>' +
-        '' +
-        '</html>';
+
+    document.body.appendChild(self);
+    var contentDoc = self.document || self.contentDocument || self.contentWindow.documnt;
+    contentDoc.open();
+    contentDoc.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n' +
+        '<html>\n' +
+        '<head>\n' +
+        '	<title>CodePress - Real Time Syntax Highlighting Editor written in JavaScript</title>\n' +
+        '	<meta name="description" content="CodePress - source code editor window" />\n' +
+        '\n' +
+        '	<script type="text/javascript">\n' +
+        '	var language = "gm";\n' +
+        '	var engine = "older";\n' +
+        '	var ua = navigator.userAgent;\n' +
+        '	var ts = (new Date).getTime(); // timestamp to avoid cache\n' +
+        '	var lh = location.href;\n' +
+        '	\n' +
+        '	if(ua.match("MSIE")) engine = "msie";\n' +
+        '	else if(ua.match("KHTML")) engine = "khtml";\n' +
+        '	else if(ua.match("Opera")) engine = "opera";\n' +
+        '	else if(ua.match("Gecko")) engine = "gecko";\n' +
+        '\n' +
+        '	if(lh.match("language=")) language = lh.replace(/.*language=(.*?)(&.*)?$/,"$1");\n' +
+        '	\n' +
+        '	// XSS fix (wwen)\n' +
+        '	// these are the only languages that GGE supports\n' +
+        '	if (!language.match(/^(css|gadget|javascript)$/)) {\n' +
+        '	  language = "gadget";\n' +
+        '	}\n' +
+        '\n' +
+        '	document.write(\'<link type="text/css" href="' + CodePress.path + 'codepress.css?ts=\'+ts+\'" rel="stylesheet" />\');\n' +
+        '	document.write(\'<link type="text/css" href="' + CodePress.path + 'languages/\'+language+\'.css?ts=\'+ts+\'" rel="stylesheet" id="cp-lang-style" />\');\n' +
+        '	document.write(\'<scr\'+\'ipt type="text/javascript" src="' + CodePress.path + 'engines/\'+engine+\'.js?ts=\'+ts+\'"></scr\'+\'ipt>\');\n' +
+        '	document.write(\'<scr\'+\'ipt type="text/javascript" src="' + CodePress.path + 'languages/\'+language+\'.js?ts=\'+ts+\'"></scr\'+\'ipt>\');\n' +
+        '	</script>\n' +
+        '\n' +
+        '</head>\n' +
+        '\n' +
+        '<script type="text/javascript">\n' +
+        'if (engine == "gecko") document.write("<body> </body>");\n' +
+        'else if(engine == "msie") document.write("<body><pre></pre></body>");\n' +
+        'else if(engine == "opera") document.write("<body></body>");\n' +
+        '// else if(engine == "khtml") document.write("<body> </body>");\n' +
+        '</script>\n' +
+        '\n' +
+        '</html>');
+    contentDoc.close();
+
     if(self.attachEvent) self.attachEvent('onload',self.initialize);
     else self.addEventListener('load',self.initialize,false);
   }
