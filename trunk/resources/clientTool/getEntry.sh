81458bc9e9cfe72703cb860ec3d501973ecbe817
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-02-12 18:32:10 +0000

    Adding the scripts to work with FSCT

---
+++ trunk/resources/clientTool/getEntry.sh
@@ -0,0 +1,2 @@
+source ./setupEnv.sh
+java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getEntry -url $FSCT_FEED_BASE/$1 -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
