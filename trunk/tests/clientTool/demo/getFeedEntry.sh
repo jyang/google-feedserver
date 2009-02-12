2eba251af53eb67dc2501d4f23cfff888e602a41
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-02-12 17:39:23 +0000

    Updating all scripts with serviceName as param name

--- trunk/tests/clientTool/demo/getFeedEntry.sh
+++ trunk/tests/clientTool/demo/getFeedEntry.sh
@@ -1,2 +1,2 @@
 source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getEntry -url $FSCT_FEED_BASE/$1 -authnServiceName $AUTHN_SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
+java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getEntry -url $FSCT_FEED_BASE/$1 -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
