f81d85fa64aa5673d47985cbeee33a4efc936d7e
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-02-12 18:30:52 +0000

    Deleting all the scripts as they will be  under resources

--- tests/clientTool/demo/insertFeedEntry.sh
+++
@@ -1,2 +0,0 @@
-source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op insert -url $FSCT_FEED_BASE/$1 -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL -entryFilePath contactFeedEntry.xml 
