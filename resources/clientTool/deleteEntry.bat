109374f99db920d7c6936f5652426a0ea23e95d6
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-02-20 01:30:43 +0000

    Adding Windows batch scripts to be used for FSCT

---
+++ resources/clientTool/deleteEntry.bat
@@ -0,0 +1,3 @@
+@echo off
+call setupEnv.bat
+java -cp %FSCT_CLASSPATH% com.google.feedserver.tools.FeedServerClientTool -op delete -url %FSCT_FEED_BASE%/%1 -serviceName %SERVICE_NAME% -authnURLProtocol %AUTHN_URL_PROTOCOL% -authnURL %AUTHN_URL%
