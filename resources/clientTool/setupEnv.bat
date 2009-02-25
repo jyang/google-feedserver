4f8c8fbe2814577873457d98744af29891b5bb1a
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-02-25 22:54:12 +0000

    Fixed FSCT scripts to set the username once and read password as input from user

--- resources/clientTool/setupEnv.bat
+++ resources/clientTool/setupEnv.bat
@@ -4,6 +4,7 @@ set FSCT_DIST_DIR=%FSCT_BASE_DIR%\dist
 set FSCT_CLASSPATH=%FSCT_DIST_DIR%/google-feedserver-java-client-2.0.jar;%FSCT_LIB_DIR%/gdata-client-1.0.jar;%FSCT_LIB_DIR%/log4j-1.2.14.jar;%FSCT_LIB_DIR%/commons-cli-1.1.jar;%FSCT_LIB_DIR%/commons-lang-2.4.jar;%FSCT_LIB_DIR%/commons-beanutils-core-1.8.0.jar;%FSCT_LIB_DIR%/commons-logging-1.0.4.jar
 
 set FSCT_DOMAIN=example.com
+set FSCT_USER_NAME=test_user@%FSCT_DOMAIN%
 
 rem Use with on-prem Google FeedServer
 set FSCT_FEED_BASE=http://localhost:8080/%FSCT_DOMAIN%
