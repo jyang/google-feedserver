773946055b35c52eebfb30bca2b49ccfc079bf83
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-02-26 00:03:09 +0000

    Updating the username with  instead of FSCT_DOMAIN

--- resources/clientTool/setupEnv.sh
+++ resources/clientTool/setupEnv.sh
@@ -4,7 +4,7 @@ FSCT_DIST_DIR=$FSCT_BASE_DIR/dist
 FSCT_CLASSPATH=$FSCT_DIST_DIR/google-feedserver-java-client-2.0.jar:$FSCT_LIB_DIR/gdata-client-1.0.jar:$FSCT_LIB_DIR/log4j-1.2.14.jar:$FSCT_LIB_DIR/commons-cli-1.1.jar:$FSCT_LIB_DIR/commons-lang-2.4.jar:$FSCT_LIB_DIR/commons-beanutils-core-1.8.0.jar:$FSCT_LIB_DIR/commons-logging-1.0.4.jar
 
 FSCT_DOMAIN=example.com
-FSCT_USER_NAME=test_user@FSCT_DOMAIN
+FSCT_USER_NAME=test_user@$FSCT_DOMAIN
 
 ### Use with on-prem Google FeedServer
 FSCT_FEED_BASE=http://localhost:8080/$FSCT_DOMAIN
