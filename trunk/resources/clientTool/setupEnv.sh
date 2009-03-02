25676bd84ea4fc8e310e230eb4f68a8aa9273168
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-03-02 18:59:51 +0000

    Updating getMapFromEntry() with a temporary work-around till a solution for escaping thesrc/java/com/google/feedserver/client/TypelessFeedServerClient.java

--- trunk/resources/clientTool/setupEnv.sh
+++ trunk/resources/clientTool/setupEnv.sh
@@ -3,17 +3,20 @@ FSCT_LIB_DIR=$FSCT_BASE_DIR/lib
 FSCT_DIST_DIR=$FSCT_BASE_DIR/dist
 FSCT_CLASSPATH=$FSCT_DIST_DIR/google-feedserver-java-client-2.0.jar:$FSCT_LIB_DIR/gdata-client-1.0.jar:$FSCT_LIB_DIR/log4j-1.2.14.jar:$FSCT_LIB_DIR/commons-cli-1.1.jar:$FSCT_LIB_DIR/commons-lang-2.4.jar:$FSCT_LIB_DIR/commons-beanutils-core-1.8.0.jar:$FSCT_LIB_DIR/commons-logging-1.0.4.jar
 
-FSCT_DOMAIN=example.com
-FSCT_USER_NAME=test_user@$FSCT_DOMAIN
+#FSCT_DOMAIN=example.com
+#FSCT_USER_NAME=test_user@$FSCT_DOMAIN
+
+FSCT_DOMAIN=joonix.net
+FSCT_USER_NAME=admin@$FSCT_DOMAIN
 
 ### Use with on-prem Google FeedServer
-FSCT_FEED_BASE=http://localhost:8080/$FSCT_DOMAIN
-SERVICE_NAME=test
-AUTHN_URL_PROTOCOL=http
-AUTHN_URL=localhost:8080
+#FSCT_FEED_BASE=http://localhost:8080/$FSCT_DOMAIN
+#SERVICE_NAME=test
+#AUTHN_URL_PROTOCOL=http
+#AUTHN_URL=localhost:8080
 
 ### Use with hosted Google FeedServer
-# FSCT_FEED_BASE=http://feedserver-enterprise.googleusercontent.com/a/$FSCT_DOMAIN/g
-# SERVICE_NAME=esp
-# AUTHN_URL_PROTOCOL=https
-# AUTHN_URL=www.google.com
+FSCT_FEED_BASE=http://feedserver-enterprise.googleusercontent.com/a/$FSCT_DOMAIN/g
+SERVICE_NAME=esp
+AUTHN_URL_PROTOCOL=https
+AUTHN_URL=www.google.com
