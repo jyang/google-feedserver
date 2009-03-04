2403ec2960a3ea9cbbb714e4b36ae081bbe446be
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-03-04 00:18:54 +0000

    Updated with scripts for managing gadget spec

--- trunk/resources/clientTool/setupEnv.bat
+++ trunk/resources/clientTool/setupEnv.bat
@@ -1,20 +1,20 @@
 set FSCT_BASE_DIR=..\..
 set FSCT_LIB_DIR=%FSCT_BASE_DIR%\lib
 set FSCT_DIST_DIR=%FSCT_BASE_DIR%\dist
-set FSCT_CLASSPATH=%FSCT_DIST_DIR%/google-feedserver-java-client-2.0.jar;%FSCT_LIB_DIR%/gdata-client-1.0.jar;%FSCT_LIB_DIR%/log4j-1.2.14.jar;%FSCT_LIB_DIR%/commons-cli-1.1.jar;%FSCT_LIB_DIR%/commons-lang-2.4.jar;%FSCT_LIB_DIR%/commons-beanutils-core-1.8.0.jar;%FSCT_LIB_DIR%/commons-logging-1.0.4.jar
+set FSCT_CLASSPATH=%FSCT_DIST_DIR%/google-feedserver-java-client-2.0.jar;%FSCT_LIB_DIR%/gdata-client-1.0.jar;%FSCT_LIB_DIR%/log4j-1.2.14.jar;%FSCT_LIB_DIR%/commons-cli-1.1.jar;%FSCT_LIB_DIR%/commons-lang-2.4.jar;%FSCT_LIB_DIR%/commons-beanutils-core-1.8.0.jar;%FSCT_LIB_DIR%/commons-logging-1.0.4.jar;%FSCT_BASE_DIR%
 
 set FSCT_DOMAIN=example.com
 set FSCT_USER_NAME=test_user@%FSCT_DOMAIN%
 
 rem Use with on-prem Google FeedServer
-set FSCT_FEED_BASE=http://localhost:8080/%FSCT_DOMAIN%
-set SERVICE_NAME=test
-set AUTHN_URL_PROTOCOL=http
-set AUTHN_URL=localhost:8080
+rem set FSCT_FEED_BASE=http://localhost:8080/%FSCT_DOMAIN%
+rem set SERVICE_NAME=test
+rem set AUTHN_URL_PROTOCOL=http
+rem set AUTHN_URL=localhost:8080
 
 rem Use with hosted Google FeedServer
-rem set FSCT_FEED_BASE=http://feedserver-enterprise.googleusercontent.com/a/%FSCT_DOMAIN%/g
-rem set SERVICE_NAME=esp
-rem set AUTHN_URL_PROTOCOL=https
-rem set AUTHN_URL=www.google.com
+set FSCT_FEED_BASE=http://feedserver-enterprise.googleusercontent.com/a/%FSCT_DOMAIN%/g
+set SERVICE_NAME=esp
+set AUTHN_URL_PROTOCOL=https
+set AUTHN_URL=www.google.com
 
