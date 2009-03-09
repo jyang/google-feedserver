95b9acedd22b56f50891a251f3f5cc58a23b4eda
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-03-09 18:45:06 +0000

    Added current directory in the class path so that the log4j.properties can be picked up

--- trunk/resources/clientTool/setupEnv.bat
+++ trunk/resources/clientTool/setupEnv.bat
@@ -1,7 +1,7 @@
 set FSCT_BASE_DIR=..\..
 set FSCT_LIB_DIR=%FSCT_BASE_DIR%\lib
 set FSCT_DIST_DIR=%FSCT_BASE_DIR%\dist
-set FSCT_CLASSPATH=%FSCT_DIST_DIR%/google-feedserver-java-client-2.0.jar;%FSCT_LIB_DIR%/gdata-client-1.0.jar;%FSCT_LIB_DIR%/log4j-1.2.14.jar;%FSCT_LIB_DIR%/commons-cli-1.1.jar;%FSCT_LIB_DIR%/commons-lang-2.4.jar;%FSCT_LIB_DIR%/commons-beanutils-core-1.8.0.jar;%FSCT_LIB_DIR%/commons-logging-1.0.4.jar;%FSCT_BASE_DIR%
+set FSCT_CLASSPATH=%FSCT_DIST_DIR%/google-feedserver-java-client-2.0.jar;%FSCT_LIB_DIR%/gdata-client-1.0.jar;%FSCT_LIB_DIR%/log4j-1.2.14.jar;%FSCT_LIB_DIR%/commons-cli-1.1.jar;%FSCT_LIB_DIR%/commons-lang-2.4.jar;%FSCT_LIB_DIR%/commons-beanutils-core-1.8.0.jar;%FSCT_LIB_DIR%/commons-logging-1.0.4.jar;.
 
 set FSCT_DOMAIN=example.com
 set FSCT_USER_NAME=test_user@%FSCT_DOMAIN%
