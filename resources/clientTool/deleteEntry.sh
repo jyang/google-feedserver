ad6f7d3843dd8c4936c9b4acf0350d1d7ae8ef92
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-03-09 18:46:36 +0000

    Set the syntax to be 'feedname entryname' rather than 'feedname/entryname'

--- resources/clientTool/deleteEntry.sh
+++ resources/clientTool/deleteEntry.sh
@@ -1,2 +1,2 @@
 source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op delete -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL -url $FSCT_FEED_BASE/$1
+java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op delete -url $FSCT_FEED_BASE/$1/$2 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
