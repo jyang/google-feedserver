099ea43df1171b2b1e72e9607e4ad521c92cfc08
rakeshs101981 <rakeshs101981@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-03-09 23:30:23 +0000

    Updated to accept feedname and entry name separately

--- resources/clientTool/updateEntry.sh
+++ resources/clientTool/updateEntry.sh
@@ -1,2 +1,2 @@
 source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op update -url $FSCT_FEED_BASE/$1 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL -entryFilePath contactEntryUpdate.xml
+java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op update -url $FSCT_FEED_BASE/$1/$2 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL -entryFilePath contactEntryUpdate.xml
