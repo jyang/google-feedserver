23b09929ec554b484c912bca81ff00c296d4b6da
jyang825@gmail.com <jyang825@gmail.com@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-03-18 00:00:46 +0000

    added constant FEED_SERVER_HOST used spinner URL that is https proxy friendly added directory preview added GadgetDirectory class added rudimentary ACL UI added better domain detection to accommodate when there is a port number in URL added saving of gadget filter

--- resources/gadgets/domain-gadget-directory-manager/code.js
+++ resources/gadgets/domain-gadget-directory-manager/code.js
@@ -46,6 +46,53 @@ function noCache(query) {
   return query;
 };
 
+function bind(fn, selfObj, var_args) {
+  var boundArgs = fn.boundArgs_;
+
+  if (arguments.length > 2) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    if (boundArgs) {
+      args.unshift.apply(args, boundArgs);
+    }
+    boundArgs = args;
+  }
+
+  selfObj = fn.boundSelf_ || selfObj;
+  fn = fn.boundFn_ || fn;
+
+  var newfn;
+  var context = selfObj || goog.global;
+
+  if (boundArgs) {
+    newfn = function() {
+      // Combine the static args and the new args into one big array
+      var args = Array.prototype.slice.call(arguments);
+      args.unshift.apply(args, boundArgs);
+      return fn.apply(context, args);
+    };
+  } else {
+    newfn = function() {
+      return fn.apply(context, arguments);
+    };
+  }
+
+  newfn.boundArgs_ = boundArgs;
+  newfn.boundSelf_ = selfObj;
+  newfn.boundFn_ = fn;
+
+  return newfn;
+};
+
+Function.prototype.bind = function(selfObj, var_args) {
+  if (arguments.length > 1) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    args.unshift(this, selfObj);
+    return bind.apply(null, args);
+  } else {
+    return bind(this, selfObj);
+  }
+};
+
 // ---------
 // constants
 
@@ -60,7 +107,7 @@ var MAX_NO_SCROLL_ROWS = 20;
 var MAX_SCROLLBAR_HEIGHT = 360;
 var MAX_RESULTS = 20;
 
-var SPINNER = '<img src="//www-opensocial.googleusercontent.com/gadgets/proxy/refresh=3600&container=enterprise&gadget=http%3A%2F%2Fwww.svca.cc%2Fdgdm.xml%3F1908234782374/http%3A%2F%2Fgoogle-feedserver.googlecode.com%2Fsvn%2Ftrunk%2Fresources%2Fgadgets%2Fprivate-gadget-editor%2Fspinner.gif">';
+var SPINNER = '<img src="//www-opensocial.googleusercontent.com/gadgets/proxy/refresh=3600&container=enterprise&gadget=http%3A%2F%2Fgoogle-feedserver.googlecode.com%2Fsvn%2Ftrunk%2Fresources%2Fgadgets%2Fdomain-gadget-directory-manager%2Fspec.xml/http%3A%2F%2Fgoogle-feedserver.googlecode.com%2Fsvn%2Ftrunk%2Fresources%2Fgadgets%2Fdomain-gadget-directory-manager%2Fspinner.gif">';
 
 var NO_FILTER = 'noRestrictions';
 var WHITE_LIST_FILTER = 'whiteList';
@@ -68,8 +115,12 @@ var BLACK_LIST_FILTER = 'blackList';
 
 var EMPTY_RESPONSE = 'empty response';
 
+var FEED_SERVER_HOST = '//feedserver-enterprise.googleusercontent.com/';
+
 // ----------------
 // global variables
+
+var domainName = '';
 
 var privateGadgets = null;
 
@@ -101,6 +152,8 @@ domainFilterListedGadgets[BLACK_LIST_FILTER] = [];
 var domainFilterListedGadgetFeedUrl = {};
 domainFilterListedGadgetFeedUrl[WHITE_LIST_FILTER] = null;
 domainFilterListedGadgetFeedUrl[BLACK_LIST_FILTER] = null;
+
+var aclFeedUrl = null;
 
 // -----------------
 // private directory
@@ -372,7 +425,7 @@ function initDirectoryFilter() {
   if (directoryFilter == null) {
     var service = createService();
     var domainConfigSettingsUrl = location.protocol +
-        '//feedserver-enterprise.googleusercontent.com/a/' + detectDomainName() +
+        FEED_SERVER_HOST + 'a/' + detectDomainName() +
         '/g/gadgets/settings';
     service.getEntry(domainConfigSettingsUrl, function(response) {
       showElementBusy('directory-filter-spinner', false);
@@ -412,7 +465,7 @@ function changeDirectoryFilter() {
       type: 'application/xml', entity: {gadgetFilter: directoryFilter}}};
   var service = createService();
   var domainConfigSettingsUrl = location.protocol +
-      '//feedserver-enterprise.googleusercontent.com/a/' + detectDomainName() +
+      FEED_SERVER_HOST + 'a/' + detectDomainName() +
       '/g/gadgets/settings';
   service.updateEntry(domainConfigSettingsUrl, entry, function(response) {
     showElementBusy('directory-filter-spinner', false);
@@ -693,14 +746,222 @@ function loadGadgetFilterList(continuation) {
 // -----------------
 // directory preview
 
-var previewingDirectory = null;
-
-function previewDirectory(directory) {
-  previewingDirectory = directory;
-  showDirectoryPreview();
+function showDirectoryPreview(directory) {
+  var gadgetDirectory = new GadgetDirectory(directory == 'public' ?
+      domainPublicGadgetFeedUrl : privateGadgetFeedUrl, directory + '-directory-preview');
+  if (directory == 'public') {
+    show('public-directory-preview');
+    show('public-directory-preview-spinner');
+    hide('private-directory-preview');
+    hide('private-directory-preview-spinner');
+  } else {
+    hide('public-directory-preview');
+    hide('public-directory-preview-spinner');
+    show('private-directory-preview');
+    show('private-directory-preview-spinner');
+  }
+  gadgetDirectory.show();
 };
 
-function showDirectoryPreview() {
+function GadgetDirectory(feedUrl, contentElementId) {
+  this.gadgets_ = [];
+  this.feedUrl_ = feedUrl;
+  this.startIndex_ = 1;
+  this.maxResults_ = MAX_RESULTS;
+  this.lastSearchTerm_ = null;
+  this.contentElementId_ = contentElementId;
+};
+
+GadgetDirectory.prototype.showWidgets = function() {
+/*
+  hide('add-gadget');
+  show('hide-directory');
+  show('public-gadget-list-dialog');
+  gadgets.window.adjustHeight();
+*/
+};
+
+GadgetDirectory.prototype.getSearchTerm = function() {
+  var searchTermElement = $(this.contentElementId_ + '-search-term');
+  return searchTermElement ? trim(searchTermElement.value) : '';
+};
+
+GadgetDirectory.prototype.setContent = function(content) {
+  $(this.contentElementId_).innerHTML = content;
+};
+
+GadgetDirectory.prototype.show = function(loadMore) {
+  this.showWidgets(); 
+
+  var searchTerm = this.getSearchTerm();
+  if (searchTerm != this.lastSearchTerm_) {
+    this.gadgets_ = [];
+    this.startIndex_ = 1;
+    this.lastSearchTerm_ = searchTerm;
+    this.setContent('');
+  }
+
+  this.load(searchTerm, loadMore, function() {
+    var html = [];
+    for (var i = 0; i < this.gadgets_.length; i++) {
+      var gadget = this.gadgets_[i].content.entity;
+      var description = typeof(gadget.description) == 'string' ? gadget.description : gadget.title;
+      html.push('<div class="list-item">',
+          '<div class="gadget-icon"><img src="', typeof(gadget.thumbnail) == 'string' ?
+          gadget.thumbnail : 'http://www.google.com/ig/images/no_image/no_image_gadget_thm.png',
+          '"></div><div class="gadget-description"><h2>', gadget.title, '</h2><p>',
+          description, '</p><p>', gadget.author ? gadget.author : '',
+          gadget.author_email ? ' (' + gadget.author_email + ')' : '', ' - ', gadget.url,
+          '</p></div></div>');
+    }
+    var gadgetListElement = $(this.contentElementId_);
+    if (gadgetListElement) {
+      gadgetListElement.style.height = MAX_SCROLLBAR_HEIGHT + 'px';
+      gadgetListElement.innerHTML = html.join('');
+      gadgets.window.adjustHeight();
+
+      if (this.gadgets_.length <= this.maxResults_) {
+        // first chunk: add listener
+        if (gadgetListElement.addEventListener) {
+          gadgetListElement.addEventListener('scroll',
+              this.handleGadgetListScroll.bind(this), false);
+        } else {
+          gadgetListElement.attachEvent('onscroll',
+              this.handleGadgetListScroll.bind(this));
+        }
+      }
+    }
+  }.bind(this));
+};
+
+GadgetDirectory.prototype.showBusy = function(busy) {
+  showElementBusy(this.contentElementId_ + '-spinner', busy);
+};
+
+GadgetDirectory.prototype.load = function(searchTerm, loadMore, continuation) {
+  if (this.gadgets_.length == 0 || loadMore) {
+    var service = createService();
+    var query = new google.gdata.client.Query(this.feedUrl_);
+    query.setParam('start-index', this.startIndex_);
+    query.setParam('max-results', this.maxResults_);
+    if (searchTerm) {
+      query.setParam('q', searchTerm);
+    }
+    service.getFeed(query, function(response) {
+      this.showBusy(false);
+      if (response) {
+        var more = response.feed.entry || [];
+        this.gadgets_ = this.gadgets_.concat(more);
+        this.startIndex_ += more.length;
+        continuation();
+      }
+    }.bind(this), function(error) {
+      this.showBusy(false);
+      showMessage(error);
+    });
+    this.showBusy(true);
+  } else {
+    this.showBusy(false);
+    continuation();
+  }
+};
+
+GadgetDirectory.prototype.handleGadgetListScroll = function(e) {
+  var target = e.target || e.srcElement;
+  if ((target.scrollTop + target.clientHeight) >= target.scrollHeight) {
+    // at the bottom of the scroll bar
+    this.show(true);
+  }
+};
+
+// --------------
+// access control
+
+var aclEntry = {
+  'PrivateGadgetSpec-acl': null,
+  'PrivateGadget-acl': null
+};
+
+function loadAcl(feedName, inputId) {
+  var aclEntryName = feedName + '-acl';
+  var service = createService();
+  service.getEntry(aclFeedUrl + '/' + aclEntryName, function(response) {
+    aclEntry[aclEntryName] = response.entry;
+  });
+};
+
+function initAccessControlTab() {
+  $('acl-domain-name1').innerHTML = domainName;
+  $('acl-domain-name2').innerHTML = domainName;
+
+  loadAcl('PrivateGadgetSpec', 'spec-group');
+  loadAcl('PrivateGadget', 'directory-group');
+};
+
+function saveAcl(inputId, feedName) {
+  var aclEntryName = feedName + '-acl';
+  var groupName = $(inputId).value;
+  if (groupName) {
+    var groupEmail = groupName + '@' + domainName;
+    var entity = {
+       name: aclEntryName, 
+       resourceInfo: {
+         resourceType: 'feed',
+         resourceRule: feedName
+       },
+       authorizedEntities: [{
+         operation: 'create',
+         entities: ['DOMAIN_ADMIN', groupEmail]
+       }, {
+         operation: 'retrieve',
+         entities: ['DOMAIN_USERS']
+       }, {
+         operation: 'update',
+         entities: ['DOMAIN_ADMIN', groupEmail]
+       }, {
+         operation: 'delete',
+         entities: ['DOMAIN_ADMIN', groupEmail]
+       }]
+    };
+
+    var entry = aclEntry[aclEntryName] ? aclEntry[aclEntryName] : {
+      xmlns: 'http://www.w3.org/2005/Atom', content: {
+        type: 'application/xml',
+        entity: null
+      }
+    };
+    entry.content.entity = entity;
+
+    var service = createService();
+    if (aclEntry[aclEntryName]) {
+      service.updateEntry(aclFeedUrl + '/' + aclEntryName, entry, function(response) {
+      }, function(error) {
+      });
+    } else {
+      service.insertEntry(aclFeedUrl, entry, function(response) {
+      }, function(error) {
+      });
+    }
+  } else {
+    // group name empty
+    if (aclEntry[aclEntryName]) {
+      // delete if exists
+      var service = createService();
+      service.deleteEntry(aclFeedUrl + '/' + aclEntryName, function(response) {
+      }, function(error) {
+      });
+    }
+  }
+};
+
+function saveAcls() {
+  saveAcl('spec-group', 'PrivateGadgetSpec');
+  saveAcl('directory-group', 'PrivateGadget');
+};
+
+function clearAcls() {
+  $('spec-group').value = '';
+  $('directory-group').value = '';
 };
 
 // -----
@@ -724,8 +985,8 @@ function detectDomainName() {
     if (params[i].indexOf('parent=') == 0) {
       var p = params[i].split('=');
       var parent = decodeURIComponent(p[1]);
-      var r = /google.com\/a\/([^\/]*)\//.exec(parent);
-      var domainName = r ? r[1] : null;
+      var r = /google.com(:[0-9]+)?\/a\/([^\/]*)\//.exec(parent);
+      var domainName = r ? r[2] : null;
       return domainName;
     }
   }
@@ -735,15 +996,15 @@ function detectDomainName() {
 
 function initGadget() {
   var tabset = new gadgets.TabSet(null, 'Private Directory');
-  tabset.alignTabs('left', 2);
-  tabset.addTab('Public Directory', {
-      contentContainer: $('tab-public'),
-      callback: function() {
-        initDirectoryFilter();
-        gadgets.window.adjustHeight();
-      },
-      tooltip: 'manage domain public gadget directory'
-  });
+  tabset.alignTabs('left', 1);
+//   tabset.addTab('Public Directory', {
+//       contentContainer: $('tab-public'),
+//       callback: function() {
+//         initDirectoryFilter();
+//         gadgets.window.adjustHeight();
+//       },
+//       tooltip: 'manage domain public gadget directory'
+//   });
   tabset.addTab('Private Directory', {
       contentContainer: $('tab-private'),
       callback: gadgets.window.adjustHeight(),
@@ -759,26 +1020,33 @@ function initGadget() {
 //      callback: gadgets.window.adjustHeight(),
 //      tooltip: 'preview domain public and private directory'
 //  });
+//   tabset.addTab('Access Control', {
+//       contentContainer: $('tab-access-control'),
+//       callback: function() {
+//         initAccessControlTab();
+//         gadgets.window.adjustHeight();
+//       }, tooltip: 'control access to private gadget spec and directory feed'
+//   });
 
-  var domainName = detectDomainName();
+  domainName = detectDomainName();
   if (domainName) {
-    publicGadgetFeedUrl = location.protocol + '//feedserver-enterprise.googleusercontent.com/Gadget';
-    domainPublicGadgetFeedUrl = location.protocol + '//feedserver-enterprise.googleusercontent.com/a/' +
-        domainName + '/Gadget';
-    domainPublicGadgetFeedUrl = location.protocol + '//feedserver-enterprise.googleusercontent.com/a/' +
-        domainName + '/PrivateGadget';
+    publicGadgetFeedUrl = location.protocol + FEED_SERVER_HOST + 'Gadget';
+    domainPublicGadgetFeedUrl = location.protocol + FEED_SERVER_HOST + 'a/' +
+        domainName + '/g/Gadget';
     domainFilterListedGadgetFeedUrl[WHITE_LIST_FILTER] =
-       location.protocol + '//feedserver-enterprise.googleusercontent.com/a/' + domainName +
+       location.protocol + FEED_SERVER_HOST + 'a/' + domainName +
        '/g/WhiteListedGadget';
     domainFilterListedGadgetFeedUrl[BLACK_LIST_FILTER] =
-        location.protocol + '//feedserver-enterprise.googleusercontent.com/a/' + domainName +
+        location.protocol + FEED_SERVER_HOST + 'a/' + domainName +
         '/g/BlackListedGadget';
-    privateGadgetFeedUrl = location.protocol + '//feedserver-enterprise.googleusercontent.com/a/' +
+    privateGadgetFeedUrl = location.protocol + FEED_SERVER_HOST + 'a/' +
         domainName + '/g/PrivateGadget';
-    privateGadgetSpecFeedUrl = location.protocol + '//feedserver-enterprise.googleusercontent.com/a/' +
+    privateGadgetSpecFeedUrl = location.protocol + FEED_SERVER_HOST + 'a/' +
         domainName + '/g/PrivateGadgetSpec';
-    privateGadgetCategoryFeedUrl = location.protocol + '//feedserver-enterprise.googleusercontent.com/a/' +
+    privateGadgetCategoryFeedUrl = location.protocol + FEED_SERVER_HOST + 'a/' +
         domainName + '/g/PrivateGadgetCategory';
+    aclFeedUrl = location.protocol + FEED_SERVER_HOST + 'a/' +
+        domainName + '/g/acl';
   } else {
     showMessage('Please set user preference "Domain Name"');
   }
