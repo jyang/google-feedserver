8faf2c241f0e4e040d6ffe1ff666643740ab4351
jyang825@gmail.com <jyang825@gmail.com@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-03-18 21:23:05 +0000

    added nocache=1

--- resources/clientTool/getEntry.sh
+++ resources/clientTool/getEntry.sh
@@ -1,2 +1,2 @@
 source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getEntry -url $FSCT_FEED_BASE/$1/$2 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
+java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getEntry -url $FSCT_FEED_BASE/$1/$2?nocache=1 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
