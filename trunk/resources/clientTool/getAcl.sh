9ccfda7fff0ff970f01998b24758adc569ff65e6
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-06-03 01:35:33 +0000

    Removed password (to be entered on console)

--- trunk/resources/clientTool/getAcl.sh
+++ trunk/resources/clientTool/getAcl.sh
@@ -4,4 +4,4 @@
 # e.g.: getAcl.sh Cafe/deli
 
 source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getFeed -url $FSCT_FEED_BASE/acl?bq=[resourceRule=$1]\&nocache=1 -username $FSCT_USER_NAME -password $FSCT_PASSWORD -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
+java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getFeed -url $FSCT_FEED_BASE/acl?bq=[resourceRule=$1]\&nocache=1 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
