70e2dfbde59dffda814b6343e858c15620b5b60c
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-06-03 01:07:01 +0000

    Added

---
+++ trunk/resources/clientTool/updateUserEntry.sh
@@ -0,0 +1,4 @@
+# updateEntry.sh {feedId} {entryId/entryName} {entryFilePath}
+
+source ./setupEnv.sh
+java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op update -url $FSCT_USER_FEED_BASE/$FSCT_USER_NAME/g/$1/$2 -username $FSCT_USER_NAME -password $FSCT_PASSWORD -entryFilePath $3 -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
