088c7ff16dbd9df5edd4764567d31c47fc9e310f
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-06-03 01:15:46 +0000

    Removed in favor of better uploadGadget.*

--- trunk/resources/clientTool/publishGadget.sh
+++
@@ -1,2 +0,0 @@
-source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.hosted.HostedFeedServerClientTools -op publish -url $FSCT_FEED_BASE/$1 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL -entryFilePath publishGadgetSpecEntity.xml -publishFeedEntryURL $FSCT_FEED_BASE/$2
