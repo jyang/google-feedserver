f6faefe84864f74a23d963dd70d1e216d334bb07
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-06-03 00:39:03 +0000

    Fixed parameter passing

--- trunk/resources/clientTool/updateEntry.bat
+++ trunk/resources/clientTool/updateEntry.bat
@@ -1,3 +1,5 @@
+rem updateEntry {feedId} {entryId} {entryFilePath}
+
 @echo off
 call setupEnv.bat
-java -cp %FSCT_CLASSPATH% com.google.feedserver.tools.FeedServerClientTool -op update -url %FSCT_FEED_BASE%/%1/%2 -username %FSCT_USER_NAME% -serviceName %SERVICE_NAME% -authnURLProtocol %AUTHN_URL_PROTOCOL% -authnURL %AUTHN_URL% -entryFilePath contactEntryUpdate.xml
+java -cp %FSCT_CLASSPATH% com.google.feedserver.tools.FeedServerClientTool -op update -url %FSCT_FEED_BASE%/%1/%2 -username %FSCT_USER_NAME% -serviceName %SERVICE_NAME% -authnURLProtocol %AUTHN_URL_PROTOCOL% -authnURL %AUTHN_URL% -entryFilePath %3
