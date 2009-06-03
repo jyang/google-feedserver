6f39a71ca2973b5b2f685e983ba06b2999ef24f2
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-06-03 01:17:08 +0000

    Removed

--- resources/clientTool/insertGadgetSpec.sh
+++
@@ -1,2 +0,0 @@
-source ./setupEnv.sh
-java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op insertGadgetSpec -url $FSCT_FEED_BASE/$1 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL -gadgetSpecEntityFile gadgetSpecEntity.xml -gadgetName $2 -gadgetSpecFile $3
