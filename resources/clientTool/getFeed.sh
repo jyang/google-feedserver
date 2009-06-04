2ddd68863a0ed103e6d9da362cd5b26ad2284a6d
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-06-04 22:11:13 +0000

    Added documentation. Removed password (to require entering it at prompt).

--- resources/clientTool/getFeed.sh
+++ resources/clientTool/getFeed.sh
@@ -1,2 +1,4 @@
+# getFeed.sh {feedName}
+
 source ./setupEnv.sh
 java -cp $FSCT_CLASSPATH com.google.feedserver.tools.FeedServerClientTool -op getFeed -url $FSCT_FEED_BASE/$1?nocache=1 -username $FSCT_USER_NAME -serviceName $SERVICE_NAME -authnURLProtocol $AUTHN_URL_PROTOCOL -authnURL $AUTHN_URL
