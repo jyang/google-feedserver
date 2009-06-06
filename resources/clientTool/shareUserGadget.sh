ce9acb260768e3dbb800df657ea6206057874c28
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-06-06 00:36:04 +0000

    Added

---
+++ resources/clientTool/shareUserGadget.sh
@@ -0,0 +1,19 @@
+# shareUserGadget.sh <gadgetName> (<principal> )+
+# e.g.: shareUserGadget.sh hello.xml one@example.com two@example.com
+
+source ./setupEnv.sh
+
+gadgetName=$1
+acl=""
+
+until [ -z "$2" ]
+do
+  if [ -z "$acl" ]; then
+    acl="r+"$2
+  else
+    acl=$acl",r+"$2
+  fi
+  shift
+done
+
+./setUserAcl.sh PrivateGadgetSpec/$gadgetName $acl
