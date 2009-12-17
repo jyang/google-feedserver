d302129fedc3e0569167789ccf85fcd30768a60a
jyang825 <jyang825@637970fa-c437-0410-ae90-df9b3b4bc8ff>
2009-12-17 19:28:01 +0000

    Sync'd to latest GData Java Client Library (1.40.2).

--- resources/clientTool/fsct.bat
+++ resources/clientTool/fsct.bat
@@ -3,6 +3,6 @@ set FSCT_BASE_DIR=%0\..
 set FSCT_BIN_DIR=%FSCT_BASE_DIR%
 set FSCT_LIB_DIR=%FSCT_BASE_DIR%\lib
 set FSCT_DIST_DIR=%FSCT_BASE_DIR%\dist
-set FSCT_CLASSPATH=%FSCT_DIST_DIR%\google-feedserver-java-client-2.0.jar;%FSCT_LIB_DIR%\gdata-client-1.0.jar;%FSCT_LIB_DIR%\log4j-1.2.14.jar;%FSCT_LIB_DIR%\commons-cli-1.1.jar;%FSCT_LIB_DIR%\commons-lang-2.4.jar;%FSCT_LIB_DIR%\commons-beanutils-core-1.8.0.jar;%FSCT_LIB_DIR%\commons-beanutils-1.8.0.jar;%FSCT_LIB_DIR%\commons-logging-1.0.4.jar;%FSCT_LIB_DIR%\commons-collections-3.2.1.jar;%FSCT_LIB_DIR%\jline-0.9.94.jar;%FSCT_BIN_DIR%
+set FSCT_CLASSPATH=%FSCT_DIST_DIR%\google-feedserver-java-client-2.0.jar;%FSCT_LIB_DIR%\gdata-client-1.0.jar;%FSCT_LIB_DIR%\google-collect-1.0-rc1.jar;%FSCT_LIB_DIR%\log4j-1.2.14.jar;%FSCT_LIB_DIR%\commons-cli-1.1.jar;%FSCT_LIB_DIR%\commons-lang-2.4.jar;%FSCT_LIB_DIR%\commons-beanutils-core-1.8.0.jar;%FSCT_LIB_DIR%\commons-beanutils-1.8.0.jar;%FSCT_LIB_DIR%\commons-logging-1.0.4.jar;%FSCT_LIB_DIR%\commons-collections-3.2.1.jar;%FSCT_LIB_DIR%\jline-0.9.94.jar;%FSCT_BIN_DIR%
 
 java -cp %FSCT_CLASSPATH% com.google.feedserver.tools.FeedClient %*
